0e29314d4d2719350d430f71c593c919246bf76e
Deepak Rai <deep.k.rai@gmail.com>
2018-09-06 16:28:03 +0530

    initial Commit

---
+++ DoFactory/Create/AbstractFactory2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoFactory.Create
+{
+    class AbstractFactory2
+    {
+        static void Main()
+        {
+
+        }
+    }
+
+
+
+    public abstract class Animal
+    {
+        public abstract string Speak();
+    }
+
+    public class Dog : Animal
+    {
+
+        public override string Speak()
+        {
+            return "Bho Bho!!!";
+        }
+    }
+
+    public class Cat : Animal
+    {
+
+        public override string Speak()
+        {
+            return "Myaaoo Myaaoo!!!";
+        }
+    }
+}
